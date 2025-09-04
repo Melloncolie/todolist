e64ca99cf9010b961fb5750cc422b3b3d6619680
Melloncolie <148479662+Melloncolie@users.noreply.github.com>
2025-09-04 21:06:20 +0300

    Update todoStorage.go

--- pkg/todo/todoStorage.go
+++ pkg/todo/todoStorage.go
@@ -7,33 +7,6 @@ import (
 	"time"
 )
 
-// func (todoStorage *TodoStorage) getStorage() (filename string, lastID int, complTask int, err error) {
-// 	var jsonData []byte
-// 	jsonData, err = os.ReadFile("file/Storage.json")
-// 	if err != nil {
-// 		return "", 0, 0, fmt.Errorf("Read JSON file/Storage.json has error: %v", err)
-// 	}
-// 	err = json.Unmarshal(jsonData, &todoStorage)
-// 	if err != nil {
-// 		return "", 0, 0, fmt.Errorf("Unmarshal JSON file/Storage.json has error: %v", err)
-// 	}
-// 	return todoStorage.Filename, todoStorage.LastID, todoStorage.ComplTasks, err
-// }
-
-// func (todoStorage *TodoStorage) writeStorage() (err error) {
-// 	var jsonData []byte
-// 	todoStorage.LastID++
-// 	jsonData, err = json.Marshal(todoStorage)
-// 	if err != nil {
-// 		return fmt.Errorf("Marshal JSON file/Storage.json has error: %v", err)
-// 	}
-// 	err = os.WriteFile("file/Storage.json", jsonData, 0644)
-// 	if err != nil {
-// 		return fmt.Errorf("Write JSON file/Storage.json has error: %v", err)
-// 	}
-// 	return
-// }
-
 func (todoStorage *TodoStorage) Import() (err error) {
 	var (
 		jsonData []byte
